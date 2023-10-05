4c3d0c743e51dd8a1eee1399c01b5092c1705dd8
Ernesto Padilla Rojas <out_epadillar@findep.dev>
2023-10-05 16:34:52 -0600

    Ajustes para sonar

--- src/main/java/com/prueba/adapter/controllers/SolicitudesController.java
+++ src/main/java/com/prueba/adapter/controllers/SolicitudesController.java
@@ -1,14 +1,19 @@
 package com.prueba.adapter.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.prueba.application.service.SolicitudesManagementService;
+import com.prueba.domain.model.ActulizarStatusSolicitud;
+import com.prueba.domain.model.Credito;
 import com.prueba.domain.model.credit_requests;
 import com.prueba.exceptions.ClienteNotFoundEception;
+import com.prueba.exceptions.SolicitudNotFoundExcepetion;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -23,14 +28,33 @@ public class SolicitudesController {
 	
 	   @PostMapping
 	    @ApiOperation(value = "Registrar una nueva solicitud")
-	    public void registrarSolicitud(@RequestBody credit_requests solicitudDto) {
+	    public ResponseEntity<Object> registrarSolicitud(@RequestBody credit_requests solicitudDto) {
 		   try {
 			solicitudesService.registrarSolicitud(solicitudDto);
+			return new ResponseEntity<>(solicitudDto, HttpStatusCode.valueOf(200));
 		} catch (ClienteNotFoundEception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			return new ResponseEntity<>(HttpStatusCode.valueOf(401));
 		}
 	    }
+	   @PostMapping
+	    @ApiOperation(value = "Dispersar una solicitud")
+	    public ResponseEntity<Object> DispersarSolicitud(@RequestBody Credito credito) {
+		   try {
+			solicitudesService.dispersarCredito(credito);
+			return new ResponseEntity<>(credito, HttpStatusCode.valueOf(200));
+		} catch (SolicitudNotFoundExcepetion e) {
+			return new ResponseEntity<>(HttpStatusCode.valueOf(401));
 
-
+		}
+	    }
+	   @PostMapping
+	    @ApiOperation(value = "Actualizar una solicitud")
+	    public ResponseEntity<Object> actualizarEstatus(@RequestBody ActulizarStatusSolicitud atalizacion) {
+		   try {
+			solicitudesService.actualizarSolicitudEstatus(atalizacion);
+			return new ResponseEntity<>(atalizacion, HttpStatusCode.valueOf(200));
+		} catch (SolicitudNotFoundExcepetion e) {
+			return new ResponseEntity<>(HttpStatusCode.valueOf(401));
+		}
+	    }
 }
